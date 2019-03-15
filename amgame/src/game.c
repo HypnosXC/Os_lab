1efc6da189e9db9b79fd6ed590a8edef5df1abf8
tracer-nju <tracer@nju.edu.cn>
2019-03-15 14:58:58 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: c600aecee63644f4a65e86225fea93ad   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  14:58:58 up 31 min,  4 users,  load average: 0.84, 0.71, 0.52

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -71,7 +71,7 @@ int main() {
     read_key();
 	time_update();
 	for(int i=1;i<=1000000;i++)
-		for(int j=1;j<=1000;j++)
+		for(int j=1;j<=10900;j++)
 			if(i>=1000000)
 				puts(itoa(TI.now_FPS));
 	int i=0;
