5eb8c6f81f668e58fb97e96aae3ccb269ebe0221
tracer-nju <tracer@nju.edu.cn>
2019-03-20 22:00:29 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:00:29 up  3:42,  6 users,  load average: 0.36, 0.24, 0.19

--- libco/co.c
+++ libco/co.c
@@ -104,13 +104,13 @@ void co_wait(struct co *thd) {
 	struct co * rc=current;
 	current->sleep=1;
 	if(thd->sleep||thd->back!=NULL)	{
-		printf("wait for dead  or sleeping %d thd!",thd->sleep);			
+		//printf("wait for dead  or sleeping %d thd!",thd->sleep);			
 		assert(0);
 	}
 	thd->back=current;
 	current=thd;			
 	if(!thd->start)	{
-		printf("goes to func");
+	//	printf("goes to func");
 		thd->start=1;				
 		thd->par=rc;
 		co_func(thd);
