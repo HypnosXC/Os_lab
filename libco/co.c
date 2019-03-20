0a4cf8932f0f934ad908f551d13c48a4c67598d5
tracer-nju <tracer@nju.edu.cn>
2019-03-20 21:53:14 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:53:14 up  3:35,  6 users,  load average: 0.14, 0.17, 0.17

--- libco/co.c
+++ libco/co.c
@@ -44,8 +44,7 @@ void co_init() {
 		runtines[i].back=NULL;
 		runtines[i].par=NULL;
 		runtines[i].start=0;
-		runtines[i].SP=malloc(MAX_HEAP_SIZE*sizeof(char));
-		runtines[i].SP+=MAX_HEAP_SIZE/2*sizeof(char)+SIZE_align/2;
+		runtines[i].SP=(void *)runtines[i].stack;
 //		printf("%p\n",runtines[i].SP);
 		rec_sta[rec_top++]=MAX_CO-i;
 	}
