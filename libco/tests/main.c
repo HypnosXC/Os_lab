8e48d4a6d1067fd825238ef9313c3346899c0c92
tracer-nju <tracer@nju.edu.cn>
2019-03-21 01:07:26 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  01:07:26 up  6:49,  8 users,  load average: 0.37, 0.43, 0.42

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -43,11 +43,10 @@ static void test_1() {
 static int g_running = 1;
 
 static void do_produce(Queue *queue) {
-   // assert(!q_is_full(queue));
-    printf("1");
-  //  Item *item = (Item*)malloc(sizeof(Item));
-  
-/*    if (!item) {
+   assert(!q_is_full(queue));
+   printf("1");
+   Item *item = (Item*)malloc(sizeof(Item));
+   if (!item) {
         fprintf(stderr, "New item failure\n");
         return;
     }
@@ -60,19 +59,19 @@ static void do_produce(Queue *queue) {
         return;
     }
     printf("4");
-    memset(item->data, 0, 10);*/
-    //sprintf(item->data, "libco-%d", g_count++);
-   // q_push(queue, item);
+    memset(item->data, 0, 10);
+    sprintf(item->data, "libco-%d", g_count++);
+    q_push(queue, item);
 }
 
 static void producer(void *arg) {
     Queue *queue = (Queue*)arg;
     for (int i = 0; i < 100; ) {
-     //   if (!q_is_full(queue)) {
+        if (!q_is_full(queue)) {
             // co_yield();
             do_produce(queue);
             i += 1;
-       // }
+        }
   //      co_yield();
     }
 }
@@ -102,21 +101,21 @@ static void test_2() {
 
     Queue *queue = q_new(); 
     struct co *thd1 = co_start("producer-1", producer, queue);
-    //struct co *thd2 = co_start("producer-2", producer, queue);
-   // struct co *thd3 = co_start("consumer-1", consumer, queue);
-   // struct co *thd4 = co_start("consumer-2", consumer, queue);
+    struct co *thd2 = co_start("producer-2", producer, queue);
+    struct co *thd3 = co_start("consumer-1", consumer, queue);
+    struct co *thd4 = co_start("consumer-2", consumer, queue);
     co_wait(thd1);
-   // co_wait(thd2);
+    co_wait(thd2);
     g_running = 0;
 
-//    co_wait(thd3);
-  //  co_wait(thd4);
+    co_wait(thd3);
+    co_wait(thd4);
 
-    //while (!q_is_empty(queue)) {
-     //   do_consume(queue);
-   // }
+    while (!q_is_empty(queue)) {
+        do_consume(queue);
+    }
 
-   // q_free(queue);
+    q_free(queue);
 }
 
 int main() {
