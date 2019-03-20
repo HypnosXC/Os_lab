b640b87e1516be6410d33d5c6533799b10b5c76f
tracer-nju <tracer@nju.edu.cn>
2019-03-20 22:24:52 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:24:52 up  4:06,  6 users,  load average: 0.11, 0.14, 0.16

--- libco/co.c
+++ libco/co.c
@@ -52,7 +52,7 @@ void co_init() {
 void co_func(struct co *thd)  {
 	current=thd;
 	asm volatile ("mov %0," _SP :	:"g"(thd->SP));
-//	printf("%p",current->SP);
+	printf("%p",current->SP);
 	(*(current->func))((void *)current->argc);
 	printf("teminated here!");
 	current->back->sleep=0;//wake the thd in wait
