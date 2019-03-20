d5cca25f96edb61f4d8222686ec582ac99f2275a
tracer-nju <tracer@nju.edu.cn>
2019-03-21 00:44:18 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  00:44:18 up  6:26,  8 users,  load average: 0.30, 0.31, 0.37

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -96,15 +96,12 @@ static void consumer(void *arg) {
 static void test_2() {
 
     Queue *queue = q_new(); 
-    printf("miaomiaomiao???");
     struct co *thd1 = co_start("producer-1", producer, queue);
     struct co *thd2 = co_start("producer-2", producer, queue);
     struct co *thd3 = co_start("consumer-1", consumer, queue);
     struct co *thd4 = co_start("consumer-2", consumer, queue);
-    printf("finished here");
     co_wait(thd1);
     co_wait(thd2);
-    printf("finished here");
     g_running = 0;
 
     co_wait(thd3);
