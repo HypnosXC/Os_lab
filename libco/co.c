8d2b559e7949dd9ad0fceeca2dc0ee302a778e4d
tracer-nju <tracer@nju.edu.cn>
2019-03-20 20:00:48 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  20:00:48 up  1:42,  4 users,  load average: 0.25, 0.19, 0.18

--- libco/co.c
+++ libco/co.c
@@ -49,7 +49,6 @@ void co_func(struct co *thd)  {
 	asm volatile ("mov " _SP ",%0;mov %1, " _SP :
 		  	"=g"(thd->ori_SP) :
 			"g"(thd->SP));
-	printf("%s??\n",thd->name);
 	(*(thd->func))((void *)thd->argc);
 	asm volatile("mov %0," _SP : :"g"(thd->ori_SP));
 }
