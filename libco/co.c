f9c747bc96a2c4ca3164c125e6b9c0304c91c813
tracer-nju <tracer@nju.edu.cn>
2019-03-20 23:47:06 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 2b0f1dfe8aea4781a856aa22bd52544c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  23:47:06 up  5:29,  8 users,  load average: 0.39, 0.29, 0.30

--- libco/co.c
+++ libco/co.c
@@ -19,12 +19,12 @@
 	
 
 struct co {
+	void *RSP __attribute__ ((aligned(SIZE_align)));
 	void* SP __attribute__((aligned(SIZE_align)));
 	jmp_buf buf;
 	func_t func;
 	char argc[100] __attribute__((aligned(SIZE_align)));
 	char name[100];
-//	char stack[MAX_HEAP_SIZE] __attribute__ ((aligned(SIZE_align)));
 	int sleep;
 	int start;
 	int dead;
@@ -37,25 +37,22 @@ void co_init() {
 	current=&runtines[0];
 	current->sleep=0;
 	current->dead=0;
-	asm volatile ("mov " _SP ",%0":"=g"(current->SP):);
-	for(int i=1;i<MAX_CO;i++)	{
+	for(int i=1;i<MAX_CO;i++)	{ 
 		runtines[i].sleep=1;
 		runtines[i].dead=1;
 		runtines[i].back=NULL;
 		runtines[i].par=NULL;
 		runtines[i].start=0;
 		runtines[i].SP=malloc(sizeof(char)*4096);//(void *)runtines[i].stack;
-
 		rec_sta[rec_top++]=MAX_CO-i;
 	}
 }
 void co_func(struct co *thd)  {
 	current=thd;
 	asm volatile ("mov %0," _SP :	:"g"(thd->SP));
-
 	(*(current->func))((void *)current->argc);
-
-	current->back->sleep=0;//wake the thd in wait
+	if(current->back!=NULL)
+		current->back->sleep=0;//wake the thd in wait
 	current->dead=1;//thd ends
 //	for(int i=1;i<MAX_CO;i++)
 //		if(current==&runtines[i]) {
@@ -63,13 +60,12 @@ void co_func(struct co *thd)  {
 //			break;
 //		}
 	assert(current->par!=NULL);
-	while(current->par->dead)
-		current->par=thd->par->par;
-	asm volatile("mov %0," _SP : :"g"(current->par->SP));
+	asm volatile("mov %0," _SP : :"g"(current->RSP));
 }
 struct co* co_start(const char *name, func_t func, void *arg) {
   struct co* new_co=&runtines[rec_sta[--rec_top]];
   new_co->func=func;
+  asm volatile ("mov " _SP ",%0"  : "=g" (new_co->RSP) : );
   strcpy(new_co->name,name);
   strcpy(new_co->argc,(char *)arg);
   new_co->dead=0;
@@ -78,45 +74,45 @@ struct co* co_start(const char *name, func_t func, void *arg) {
 }
 void co_yield() {
 	struct co *rc=current;
-	if(!setjmp(current->buf))	{//first return , change current
+	if(!setjmp(current->buf))	{//first return , change  current
 		for(int i=1;i<MAX_CO;i++)	{
 			if(&runtines[i]==current)	{
 				continue;
-			}
+		 	}
 			if(!runtines[i].sleep&&!runtines[i].dead)	{
 				current=&runtines[i];
 				if(current->start){
 					longjmp(current->buf,1);
-				}
+		 		}
 				else	{
 				   	current->start=1;
 					current->par=rc;	
 				 	co_func(current);
-				}
+		 		}
 				break;
-			}
-		}
+		 	}
+		} 
 	}
 	current=rc;
 }
 void co_wait(struct co *thd) {
 	struct co *rc=current;
-	current->sleep=1;
-	if(thd->sleep||thd->back!=NULL)	{
-
-		assert(0);
+	if(!setjmp(thd->buf))	{
+		current->sleep=1;
+		if(thd->sleep||thd->back!=NULL)	{
+			assert(0);
+		}
+		thd->back=current;
+		current=thd;			
+		if(!thd->start)	{
+			thd->start=1;				
+			thd->par=rc;
+			co_func(thd);
+		}
+		else {
+			longjmp(current->buf,1);
+		}
 	}
-	thd->back=current;
-	current=thd;			
-	if(!thd->start)	{
-
-		thd->start=1;				
-		thd->par=rc;
-		co_func(thd);
-	}
-	else {
-		longjmp(current->buf,1);
-	}
-
+	current=rc;
 }
 
