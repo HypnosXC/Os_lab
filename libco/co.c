5d5237989006b16c06bed136ca0238ed4d257bd5
tracer-nju <tracer@nju.edu.cn>
2019-03-21 17:00:54 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: f4c49a519c0d4f0fb68a93bd9ecd5b23   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  17:00:54 up  3:05,  6 users,  load average: 0.45, 0.54, 0.57

--- libco/co.c
+++ libco/co.c
@@ -109,7 +109,9 @@ void co_yield() {
 }
 void co_wait(struct co *thd) {
 	struct co *rc=current;
-	if(!setjmp(rc->buf))	{
+	int t;
+	while(t=setjmp(rc->buf)&&t);
+	if(!t)	{
 		rc->sleep=1;
 		if(thd->sleep||thd->back!=NULL)	{
 			assert(0);
