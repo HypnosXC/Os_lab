9f68e04a3e0551ede9b60609409f083534e36bb6
tracer-nju <tracer@nju.edu.cn>
2019-04-03 21:13:25 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 7aab58dc2d4b4f2fb4f7655fe9f1de81   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:13:25 up 22:19,  4 users,  load average: 0.39, 0.51, 0.54

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -28,10 +28,10 @@ static void* kalloc(size_t size) {
 }
 
 static void kfree(void *ptr) {
+	lock(alloc_lock);
 	int pos=pm_end-(intptr_t)ptr;
 	pos/=BLOCK_SIZE;
 	lock(printf_lk);
-	lock(alloc_lk);
 	printf("free %p at %d",ptr,pos);
 	unlock(printf_lk);
 	bt_free(pos);
