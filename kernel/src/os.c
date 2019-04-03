c50370fc02251d6f8a78e17a58e3670956ac5e00
tracer-nju <tracer@nju.edu.cn>
2019-04-03 21:25:52 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 7aab58dc2d4b4f2fb4f7655fe9f1de81   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:25:52 up 22:31,  4 users,  load average: 0.09, 0.25, 0.41

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -12,7 +12,7 @@ static void hello() {
     _putc('\n');*/
     char *mm=pmm->alloc(100);
     lock(printf_lk);
-    printf("mm is %p\n",mm);
+    printf("mm is %p\n,with cpu %d",mm,_cpu());
 	unlock(printf_lk);
     mm="maybe the alloc func is not right\n";
     pmm->free(mm);
