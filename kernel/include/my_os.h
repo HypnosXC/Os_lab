88a24de29775c38b145eed6b161c3d139bb4fb68
tracer-nju <tracer@nju.edu.cn>
2019-04-03 14:38:45 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 7aab58dc2d4b4f2fb4f7655fe9f1de81   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  14:38:45 up 15:44,  4 users,  load average: 0.60, 0.63, 0.70

--- kernel/include/my_os.h
+++ kernel/include/my_os.h
@@ -1,5 +1,5 @@
 int printf(const char *mtf,...);
-intptr_t atomic_xchg(volatile intptr_t *addr,intptr_t newval) {
+static intptr_t atomic_xchg(volatile intptr_t *addr,intptr_t newval) {
 	intptr_t result;
 	asm volatile ("lock xchg %0,%1": "+m"(*addr),"=a"(result):"1"(newval):"cc");
 	return result;
@@ -10,11 +10,11 @@ typedef struct _lock{
 }lc;
 lc bas_lc[1000];
 #define allc_lc (bas_lc+1)
-void lock(lc * lk) {
+static void lock(lc * lk) {
 	printf("locked %s\n",lk->name);
 	while(atomic_xchg(&lk->state,1));
 }
-void unlock(lc *lk) {
+static void unlock(lc *lk) {
 	printf("unlocked %s\n",lk->name);
 	atomic_xchg(&lk->state,0);
 }
