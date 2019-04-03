5c0964aadd7f3173bbe7ddca7c60ee5fb6d73aa9
tracer-nju <tracer@nju.edu.cn>
2019-04-03 22:00:14 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 7aab58dc2d4b4f2fb4f7655fe9f1de81   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  22:00:14 up 23:06,  4 users,  load average: 0.20, 0.16, 0.17

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -14,7 +14,7 @@ static void hello() {
     lock(printf_lk);
     printf("mm is %p\n,with cpu %d \n",mm,_cpu());
 	unlock(printf_lk);
-    mm="maybe the alloc func is not right\n";
+    *mm="maybe the alloc func is not right\n";
     lock(printf_lk);
     printf("mm is %p\n,with cpu %d \n",mm,_cpu());
     unlock(printf_lk);
