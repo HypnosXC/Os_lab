019b7db59ff380bf3f66f573823d00838b5ef92d
tracer-nju <tracer@nju.edu.cn>
2019-04-04 15:10:42 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ef9a2d2c33f54dc9bca96e2e15961c34   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  15:10:42 up  1:10,  4 users,  load average: 0.84, 0.69, 0.69

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -99,7 +99,7 @@ void test() {
 	 	}
 		else {
 			lock(printf_lk);
-			printf("%d,%s,cpu%d\n",i,q[top-1],_cpu());
+			printf("\n%d,%s,cpu%d\n",i,q[top-1],_cpu());
 			unlock(printf_lk);
 			pmm->free(q[--top]);
 		}
