9f81c6271f44c61cee52950d438473cf9748e348
tracer-nju <tracer@nju.edu.cn>
2019-04-08 21:06:39 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: edc39a6cdb9d4f00b03a6298fec45e00   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-46-generic       Architecture: x86-64  21:06:39 up  5:41,  6 users,  load average: 0.43, 0.45, 0.58

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -21,7 +21,7 @@ static void* kalloc(size_t size) {
 	size/=BLOCK_SIZE;
 	int pos=bt_alloc(size);
  lock(printf_lk);
- printf("alloc %d block at %d,with cpu %d\n",size,pos,_cpu());
+ printf("alloc %p block at %d,with cpu %d\n",(void *)(pm_end-pos*BLOCK_SIZE),pos,_cpu());
  unlock(printf_lk);
  	unlock(alloc_lk);
 	return (void *)(pm_end-pos*BLOCK_SIZE);
@@ -33,9 +33,9 @@ static void* kalloc(size_t size) {
 		cu_pos=bt_alloc(1);
 		current_ptr=pm_end-cu_pos*BLOCK_SIZE;	
 		off_set=size;
-		// lock(printf_lk);
-		// printf("alloc %d block at%x,with cpu %d\n",size,pm_end-pos*BLOCK_SIZE,_cpu());
-		// unlock(printf_lk);
+		 lock(printf_lk);
+		 printf("alloc %p block at%d,with cpu %d\n",pm_end-pos*BLOCK_SIZE,cu_pos,_cpu());
+		 unlock(printf_lk);
  		unlock(alloc_lk);
 	}
 	else {
