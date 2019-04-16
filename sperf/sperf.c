5daafa5f09884025e8ec666502d6368083a90c26
tracer-nju <tracer@nju.edu.cn>
2019-04-16 17:54:01 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: e54dda6948a14208aee23bd5fc3b133f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  17:54:01 up  1:11,  4 users,  load average: 2.26, 2.09, 1.75

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<assert.h>
 int filedes[2];
 int main(int argc, char *argv[]) {
   if(pipe(fildes)!=0) {
   	printf("wrong pipe operation\n");
 	assert(0);
   }
-  pid=fork();
+  int pid=fork();
   if(pid==0)	{
   	char *subargv[100];
 	subargv[0]="-T";
 	for(int i=0;i<argc;i++)
 		subargv[i+1]=argv[i];
 	subargv[argc+2]=(char *)0;
-	char * envp={0,NULL};
+	char * envp[]={0,NULL};
 	execve("/bin/strace",subargv,envp);
 	assert(0);
   }
