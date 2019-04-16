b7632e4fc9c297deef6875ddbfe4f21a6bd430f4
tracer-nju <tracer@nju.edu.cn>
2019-04-16 23:38:01 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 5f105ac7463a4d6b9e272bf253fc6556   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  23:38:02 up  1:52, 10 users,  load average: 0.51, 0.87, 1.19

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 		assert(0);
 	  }
 	  int i=0;
-	  while(fgets(data_inline,1024,fd)!=NULL)	{
+	  while(fgets(data_inline,1024,fd)!=NULL&&strlen(data_inline)!=0)	{
 	  	printf("%d=%s\n",i++,data_inline);
 	  }
 	  close(filedes[0]);
