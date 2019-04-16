d4f7a7697d64220bccb2cf8ca6669acd93a0c49e
tracer-nju <tracer@nju.edu.cn>
2019-04-17 00:29:42 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 5f105ac7463a4d6b9e272bf253fc6556   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:29:42 up  2:44, 10 users,  load average: 0.40, 0.47, 0.62

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -61,11 +61,13 @@ int main(int argc, char *argv[]) {
 	  if(dup2(filedes[0],STDIN_FILENO)<0)
 		  assert(0);
 	  double total_time=0,ts=clock(); 
-	  while(get_line(data_inline)) {
-	 //    int tl=strchr(data_inline,'(')-data_inline;
-//		 int el=strrchr(data_inline,'<')-data_inline;
-	//	 strncpy(name,data_inline,tl);
-	//	 strncpy(cost,data_inline+el+1,strlen(data_inline)-el-2);
+	  int i=100000;
+	  while(get_line(data_inline)&&i) {
+		  i--;
+	     int tl=strchr(data_inline,'(')-data_inline;
+		 int el=strrchr(data_inline,'<')-data_inline;
+		 strncpy(name,data_inline,tl);
+		 strncpy(cost,data_inline+el+1,strlen(data_inline)-el-2);
 		// printf("%s\n%s : %s\n",data_inline,name,cost);
 		 total_time+=atof(cost);
 		 int matc=0;
