6ef7c2e07e3bb8c8cedd70bf6dbfc0fce5ec5efa
tracer-nju <tracer@nju.edu.cn>
2019-04-16 21:55:02 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 5f105ac7463a4d6b9e272bf253fc6556   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  21:55:02 up 9 min,  7 users,  load average: 1.47, 0.85, 0.38

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
 	for(int i=0;i<argc+3;i++)
 		printf("%s ",subargv[i]);
 	printf("\n");
-	char * envp[]={0,NULL};
+//	char * envp[]={0,NULL};
 //	execve("/usr/bin/strace",subargv,envp);
 	assert(0);
   }
