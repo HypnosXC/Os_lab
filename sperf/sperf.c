4ef93ed399dd52a83a7a012d4fbb285bf388327c
tracer-nju <tracer@nju.edu.cn>
2019-04-16 17:52:21 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: e54dda6948a14208aee23bd5fc3b133f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  17:52:21 up  1:10,  4 users,  load average: 2.30, 2.01, 1.69

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,3 +1,22 @@
+#include<stdio.h>
+#include<unistr,h>
+int filedes[2];
 int main(int argc, char *argv[]) {
-  return 0;
+  if(pipe(fildes)!=0) {
+  	printf("wrong pipe operation\n");
+	assert(0);
+  }
+  pid=fork();
+  if(pid==0)	{
+  	char *subargv[100];
+	subargv[0]="-T";
+	for(int i=0;i<argc;i++)
+		subargv[i+1]=argv[i];
+	subargv[argc+2]=(char *)0;
+	char * envp={0,NULL};
+	execve("/bin/strace",subargv,envp);
+	assert(0);
+  }
+  else
+	  return 0;
 }
