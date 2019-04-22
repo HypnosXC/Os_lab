47258431598b2e1cc161c5329c28da40def14a0c
tracer-nju <tracer@nju.edu.cn>
2019-04-22 23:24:00 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: f5b43ac64b754aa78e1bbaddab536abf   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  23:24:00 up 31 min,  4 users,  load average: 0.63, 0.78, 0.66

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
 	  while(get_line(data_inline)&&i) {
 		 i--;
 		 fflush(stdout);
-		 printf("%s\n",data_inline);
+		// printf("%s\n",data_inline);
 	     int tl=strchr(data_inline,'(')-data_inline;
 		 int el=strrchr(data_inline,'<')-data_inline;
 		 //printf("%d %d\n",tl,el);
