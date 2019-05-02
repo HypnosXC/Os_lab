bf9afb0ce9e532867ce936460f47314af3f4f44c
tracer-nju <tracer@nju.edu.cn>
2019-05-02 20:42:54 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 10cd74a613404d69a6bc4ffcad07a691   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  20:42:55 up  3:44,  4 users,  load average: 0.43, 0.51, 0.61

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<dir.h>
 #include<dlfcn.h>
 #include<sys/types.h>
 char dat_inline[1000];
@@ -14,17 +15,25 @@ void sgetline()	{
 	}
 	*p='\0';
 }
+char dlstore[]="dl-XXXXXX";
+char gccode[1000];
 int main(int argc, char *argv[]) {
+ // dlstore=mktemp(dlstore);
   while(1) {
 	sgetline();
 	char *func=strstr(dat_inline,"int");
-	if(func==NULL||func!=dat_inline) {//expression
+	if(func ==NULL||func!=dat_inline) {//expression
 		
 	}
 	else {//function
-		FILE* fd=fopen("a.c","ab+");
+   	    FILE* fd=fopen("dl-XXXXXX.c","a+");	
 		fprintf(fd,"%s\n",dat_inline);
 		fclose(fd);
+		fprintf(gccode,"gcc %s.c -shared -fPIC -o %s.so",dlstore,dlstore);
+		system(gccode);
+		memset(gccode,0,strlen(gccode));
+		fprintf(gccode,"%s.so",dlstore);
+		dlopen(gccode,RTLD_LAZY|RTLD_GLOBAL)
 	}
   }
 //  system("gcc a.c -shared -fPIC -o test.so");
