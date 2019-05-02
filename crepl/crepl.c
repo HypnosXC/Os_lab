993d250b2435b4aff58b65bc5691f181f3eb3448
tracer-nju <tracer@nju.edu.cn>
2019-05-02 20:14:01 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 10cd74a613404d69a6bc4ffcad07a691   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  20:14:02 up  3:15,  4 users,  load average: 0.35, 0.50, 0.59

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,3 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdllib.h>
+#include<dlfcn.h>
+#include<sys/types.h>
+char dat_inline[1000];
 int main(int argc, char *argv[]) {
-  return 0;
+  while(1) {
+ 	gets_s(dat_inline,1000);
+	char *func=strstr(dat_inline,"int");
+	if(func==NULL||func!=dat_inline) {//expression
+		
+	}
+	else {//function
+		FILE* fd=fopen("a.c",O_RDWR|O_APPEND);
+		fprintf(fd,dat_inline);
+
+		fclose(fd);
+	}
+  }
+//  system("gcc a.c -shared -fPIC -o test.so");
 }
