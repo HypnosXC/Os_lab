6033313e56e4939f66be98aae528bf220d1d8e6b
tracer-nju <tracer@nju.edu.cn>
2019-05-08 00:42:30 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: a8a73b4d3a9a4d36897d0435e3ec5af3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:42:30 up  5:11,  4 users,  load average: 0.13, 0.18, 0.24

--- kernel/include/def.h
+++ kernel/include/def.h
@@ -1,4 +1,4 @@
 //spin_lock.c
 void spin_init(struct spinlock *lk,char *name);
-void spin_lock(struct *lk);
-void spin_unlock(struct *lk);
+void spin_lock(struct spinlock *lk);
+void spin_unlock(struct spinlock *lk);
