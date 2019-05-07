27cab683045151f08cdfa8dd238a5eaee0713aa2
tracer-nju <tracer@nju.edu.cn>
2019-05-08 00:21:30 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: a8a73b4d3a9a4d36897d0435e3ec5af3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:21:30 up  4:50,  4 users,  load average: 0.32, 0.30, 0.31

--- kernel/src/spin_lock.c
+++ kernel/src/spin_lock.c
@@ -4,13 +4,13 @@ void spin_init(struct spinlock *lk,char *name) {
 	lk->locked=0;
 	lk->name=name;
 }
-void spin_lock(spinlock *lk) {
+void spin_lock(struct spinlock *lk) {
 	cli();//disable interrupts
 	while(_atomic_xchg(&lk->locked,1)!=0);
 	lk->hcpu=_cpu();
 	cpu_cnt[lk->hcpu]++;
 }
-void spin_unlock(spinlock *lk) {
+void spin_unlock(struct spinlock *lk) {
 	if(_atomic_xchg(&lk->locked,0)!=1) {
 		panic("unlock but no hold!");
 	}
