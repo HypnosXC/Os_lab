4f5165204823df66c4ace3e96a198803311b35cd
tracer-nju <tracer@nju.edu.cn>
2019-05-08 15:33:46 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: a8a73b4d3a9a4d36897d0435e3ec5af3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:33:47 up 20:02,  4 users,  load average: 0.28, 0.23, 0.31

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -13,7 +13,7 @@ task_t* current_task() {
 	return current[_cpu()];
 }
 _Context* context_switch(_Event e,_Context* c) {
-	kmt->spin_lock(&tsk_lk);
+	spin_lock(&tsk_lk);
 	for(int i=_ncpu();i<32;i++)	{
 		if(current[i]!=NULL&&current[i]->state==0)	{
 		   task_t* t=current[_cpu()];
@@ -22,17 +22,17 @@ _Context* context_switch(_Event e,_Context* c) {
 		   break;	   
 		}
 	}
-	kmt->spin_unlock(&tsk_lk);
+	spin_unlock(&tsk_lk);
 	return current[_cpu()]->context;
 }
 _Context* context_save(_Event e,_Context *c) {
-	kmt->spin_lock(&tsk_lk);
+	spin_lock(&tsk_lk);
     current[_cpu()]->context=c;
-	kmt->spin_unlock(&tsk_lk);
+	spin_unlock(&tsk_lk);
 	return NULL;
 }
 int create(task_t *task,const char *name,void (*entry)(void *arg),void *arg) {
-	kmt->spin_lock(&tsk_lk);
+	spin_lock(&tsk_lk);
 	task->name=name;
 	task->state=0;
 	task->stack.start=pmm->alloc(STACK_SIZE);
@@ -42,17 +42,17 @@ int create(task_t *task,const char *name,void (*entry)(void *arg),void *arg) {
 	for(int i=0;i<32;i++)
 		if(current[i]==NULL)
 			current[i]=task;
-	kmt->spin_unlock(&tsk_lk);
+	spin_unlock(&tsk_lk);
 	return 1;
 }
 void teardown(task_t *task) {
-	kmt->spin_lock(&tsk_lk);
+	spin_lock(&tsk_lk);
 	for(int i=0;i<32;i++)
 		if(current[i]==task)
 				current[i]=NULL;
 	pmm->free(task->stack.start);
 	pmm->free(task);
-	kmt->spin_unlock(&tsk_lk);
+	spin_unlock(&tsk_lk);
 }
 void kmt_init() {
 	os->on_irq(-19999,_EVENT_NULL,context_save);
