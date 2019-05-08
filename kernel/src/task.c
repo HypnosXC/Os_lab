7ea31a223876aad5184955941fc8211d25ef5d89
tracer-nju <tracer@nju.edu.cn>
2019-05-08 15:28:56 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: a8a73b4d3a9a4d36897d0435e3ec5af3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:28:57 up 19:58,  4 users,  load average: 0.53, 0.37, 0.38

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -2,7 +2,6 @@
 #include<klib.h>
 #define STACK_SIZE 4096
 task_t *current[32];
-static int top;
 spinlock tsk_lk;
 void noreach() {
 	printf("never been here!\n");
@@ -37,25 +36,26 @@ int create(task_t *task,const char *name,void (*entry)(void *arg),void *arg) {
 	task->name=name;
 	task->state=0;
 	task->stack.start=pmm->alloc(STACK_SIZE);
-	task->stack.end=(void *)((intptr_t)task->stack->start+STACK_SIZE);
+	task->stack.end=(void *)((intptr_t)task->stack.start+STACK_SIZE);
 	task->context=_kcontext(task->stack,entry,arg);
 	printf("new task:%s\n",name);
 	for(int i=0;i<32;i++)
 		if(current[i]==NULL)
 			current[i]=task;
 	spin_unlock(&tsk_lk);
+	return 1;
 }
 void teardown(task_t *task) {
 	spin_lock(&tsk_lk);
 	for(innt i=0;i<32;i++)
 		if(current[i]==task)
 				current[i]=NULL;
-	pmm->free(task->stack->start);
+	pmm->free(task->stack.start);
 	pmm->free(task);
 	spin_unlock(&tsk_lk);
 }
 void kmt_init() {
-	os->on_irq(-19999,_ENENT_NULL,context_save);
+	os->on_irq(-19999,_EVENT_NULL,context_save);
 	os->on_irq(19999,_EVENT_NULL,context_switch);
 	for(int i=0;i<8;i++)	{
 		create(pmm->alloc(sizeof(task_t)),"empty",noreach,NULL);
