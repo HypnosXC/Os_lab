36e46fe0116d6d97b25a03b53e7bf58896406112
tracer-nju <tracer@nju.edu.cn>
2019-05-10 00:23:39 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:23:39 up 15:45,  6 users,  load average: 0.41, 0.46, 0.44

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -123,6 +123,7 @@ void sem_wait(sem_t *sem) {
 void sem_signal(sem_t *sem) {
 	spin_lock(&sem->sem_lk);
 	sem->value++;
+	printf("\033[31m sem_signal: \033[32m cpu%d for %s\033[0m",_cpu(),sem->name);
 	if(sem->value>=0)
 		sem->sem_st[sem->top--]->state=0;//runable
 	spin_unlock(&sem->sem_lk);
