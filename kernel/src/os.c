9485e34efe2103c4bec5c873f726fc76e1f4bce8
tracer-nju <tracer@nju.edu.cn>
2019-05-09 22:48:26 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  22:48:26 up 14:10,  6 users,  load average: 0.23, 0.18, 0.16

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -64,6 +64,10 @@ static _Context *os_trap(_Event ev, _Context *context) {
   }
   kmt->spin_unlock(&trap_lk);
  // printf("\033[32mtrap finished\n\033[0m");
+  if(ret==NULL) {
+  	printf("\n\033[32m panic \003[33m trap return NULL to revise\033[0m\n");
+	assert(0);
+  }
   return ret;
 }
 
