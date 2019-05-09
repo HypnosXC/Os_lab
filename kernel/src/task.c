0fa65ab1f3566938cf22a2fc5e97d505eaf0c911
tracer-nju <tracer@nju.edu.cn>
2019-05-09 14:10:11 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  14:10:11 up  5:32,  4 users,  load average: 0.57, 0.79, 0.86

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -12,17 +12,17 @@ void cli(){
 void sti() {
 	asm volatile ("sti");
 }
-void kmt_spin_init(struct spinlock *lk, char *name) {
+void spin_init(struct spinlock *lk, char *name) {
 	lk->locked=0;
 	lk->name=name;
 }
-void kmt_spin_lock(struct spinlock *lk) {
+void spin_lock(struct spinlock *lk) {
 	cli();//disable interrupts
 	while(_atomic_xchg(&lk->locked,1)!=0);
 	lk->hcpu=_cpu();
 	cpu_cnt[lk->hcpu]++;
 }
-void kmt_spin_unlock(struct spinlock *lk) {
+void spin_unlock(struct spinlock *lk) {
 	if(_atomic_xchg(&lk->locked,0)!=1) {
 		printf("%s: unlock but no hold!\n",lk->name);
 		assert(0);
@@ -42,14 +42,14 @@ void kmt_spin_unlock(struct spinlock *lk) {
 
 
 //sem started
-void kmt_sem_init(sem_t *sem, char *name,int value){
+void sem_init(sem_t *sem, char *name,int value){
 	spin_init(&sem->sem_lk,name);
 	sem->value=value;
 	sem->name=pmm->alloc(sizeof(name));
     strcpy(sem->name,name);
 	sem->top=0;
 }
-void kmt_sem_wait(sem_t *sem) {
+void sem_wait(sem_t *sem) {
 	spin_lock(&sem->sem_lk);
 	sem->value--;
 	while(sem->value<0) {
@@ -65,7 +65,7 @@ void kmt_sem_wait(sem_t *sem) {
 	}
 	spin_unlock(&sem->sem_lk);
 }
-void kmt_sem_signal(sem_t *sem) {
+void sem_signal(sem_t *sem) {
 	spin_lock(&sem->sem_lk);
 	sem->value++;
 	if(sem->value>=0)
@@ -107,7 +107,7 @@ _Context* context_save(_Event e,_Context *c) {
 	spin_unlock(&tsk_lk);
 	return NULL;
 }
-int kmt_create(task_t *task,const char *name,void (*entry)(void *arg),void *arg) {
+int create(task_t *task,const char *name,void (*entry)(void *arg),void *arg) {
 	spin_lock(&tsk_lk);
 	strcpy(task->name,name);
 	task->state=0;
@@ -121,7 +121,7 @@ int kmt_create(task_t *task,const char *name,void (*entry)(void *arg),void *arg)
 	spin_unlock(&tsk_lk);
 	return 1;
 }
-void kmt_teardown(task_t *task) {
+void teardown(task_t *task) {
 	spin_lock(&tsk_lk);
 	for(int i=0;i<32;i++)
 		if(current[i]==task)
@@ -138,4 +138,15 @@ void kmt_init() {
 		create(pmm->alloc(sizeof(task_t)),"empty",noreach,NULL);
 	} 
 	spin_init(&tsk_lk,"task");
+}
+MODULE_DEF(kmt) {
+	.init = kmt_init(),
+	.spin_init=spin_init();
+	.sem_init=sem_init();
+	.spin_lock=spin_lock();
+	.spin_unlock=spin_unlock();
+	.sem_wait=sem_wait();
+	.sem_signal=sem_signal();
+	.create=create();
+	.teardown=teardown();
 }
