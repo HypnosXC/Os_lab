233aff3da09cdc7d18224e91cb6a2d89e42e8008
tracer-nju <tracer@nju.edu.cn>
2019-05-09 14:18:12 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  14:18:12 up  5:40,  4 users,  load average: 1.88, 1.10, 0.92

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -47,7 +47,7 @@ static _Context *os_trap(_Event ev, _Context *context) {
 	}
   }
   if(ret==NULL) {
-  	printf("\033[34m fk trap no recurse!\n\033[2J");
+  	printf("\033[34m fk trap no recurse!\n\033[0m");
 	assert(0);
   }
   kmt->spin_unlock(&trap_lk);
