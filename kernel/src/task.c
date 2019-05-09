0d41b5e25f372567108e06af4c00065f6642d18b
tracer-nju <tracer@nju.edu.cn>
2019-05-10 00:33:10 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:33:10 up 15:55,  6 users,  load average: 0.40, 0.47, 0.45

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -103,10 +103,10 @@ void sem_init(sem_t *sem, const char *name,int value){
 }
 void sem_wait(sem_t *sem) {
 	spin_lock(&sem->sem_lk);
+	task_t* cur=current_task();
 	sem->value--;
-	printf("\n\033[31m sem_wait : \033[32m cpu%d for %s,stack %d\n\033[0m",_cpu(),sem->name,sem->top);
+	printf("\n\033[31m sem_wait : \033[32m cpu%d for %s,task %s\n\033[0m",_cpu(),sem->name,cur->name);
 	while(sem->value<0) {
-	  	task_t* cur=current_task();
 		if(cur->state!=1)//no sleeped before or waken but no resourse
 			sem->sem_st[sem->top++]=cur;
 		cur->park=1;//sleep;
