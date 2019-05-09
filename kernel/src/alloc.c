a53646c4eeec5a197f6b1c23469e7cf4439a6d5d
tracer-nju <tracer@nju.edu.cn>
2019-05-09 08:59:52 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  08:59:53 up 22 min,  4 users,  load average: 0.78, 0.54, 0.42

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -2,7 +2,7 @@
 #include <klib.h>
 #include <alloc.h>
 static uintptr_t pm_start, pm_end,pm_size,current_ptr=0,cu_pos,off_set=0;
-struct spinlock alloc_lc={0,0,"alloc"};
+struct spinlock alloc_lc;
 struct spinlock* alloc_lk;
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
@@ -10,32 +10,37 @@ static void pmm_init() {
   pm_size=pm_end-pm_start;
   bt_init((intptr_t *)pm_start,pm_size/(BLOCK_SIZE+32));//block used
   pm_start+=pm_size/(BLOCK_SIZE+32)*32;
+  if(kmt->spin_init==NULL) {
+  	printf("gg uninit for alloc's kmt!\n");
+	assert(0);
+  }
+  kmt->spin_init(alloc_lc,"alloc");
   alloc_lk=&alloc_lc;
 }
 
 static void* kalloc(size_t size) {
  if(size>=BLOCK_SIZE) {
- 	spin_lock(alloc_lk);
+ 	kmt->spin_lock(alloc_lk);
  	size=size+(BLOCK_SIZE-size%BLOCK_SIZE);
 	size/=BLOCK_SIZE;
 	int pos=bt_alloc(size);
 // lock(printf_lk);
 // printf("alloc %p block at %d,with cpu %d\n",(void *)(pm_end-pos*BLOCK_SIZE),pos,_cpu());
 // unlock(printf_lk);
-    spin_unlock(alloc_lk);
+    kmt->spin_unlock(alloc_lk);
 	return (void *)(pm_end-pos*BLOCK_SIZE);
  }
  else {
 	size=size+base_sz-size%base_sz;
  	if(!current_ptr||off_set+size>=BLOCK_SIZE) {
-		spin_lock(alloc_lk);
+		kmt->spin_lock(alloc_lk);
 		cu_pos=bt_alloc(1);
 		current_ptr=pm_end-cu_pos*BLOCK_SIZE;	
 		off_set=size;
 //		 lock(printf_lk);
 //		 printf("alloc %p block at%d,with cpu %d\n",pm_end-cu_pos*BLOCK_SIZE,cu_pos,_cpu());
 //		 unlock(printf_lk);
- 		spin_unlock(alloc_lk);
+ 		kmt->spin_unlock(alloc_lk);
 	}
 	else {
 //		lock(printf_lk);
@@ -49,7 +54,7 @@ static void* kalloc(size_t size) {
 }
 
 static void kfree(void *ptr) {
-	spin_lock(alloc_lk);
+	kmt->spin_lock(alloc_lk);
 	intptr_t pos=(intptr_t)ptr;
 	while(pos%BLOCK_SIZE)	{ 
 		pos-=pos%BLOCK_SIZE;
@@ -60,7 +65,7 @@ static void kfree(void *ptr) {
 //	printf("free %p at %d with cpu %d\n",ptr,pos,_cpu());
 //	unlock(printf_lk);
 	bt_free(pos);
-	spin_unlock(alloc_lk);
+	kmt->spin_unlock(alloc_lk);
 }
 
 MODULE_DEF(pmm) {
