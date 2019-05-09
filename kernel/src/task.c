510fc52490c9aed1de9049ee807f50262c22be66
tracer-nju <tracer@nju.edu.cn>
2019-05-10 00:51:23 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  00:51:23 up 16:13,  6 users,  load average: 0.67, 0.47, 0.43

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -105,7 +105,7 @@ void sem_wait(sem_t *sem) {
 	spin_lock(&sem->sem_lk);
 	task_t* cur=current_task();
 	sem->value--;
-	printf("\n\033[31m sem_wait : \033[32m cpu%d for %s,value is%d\n\033[0m",_cpu(),sem->name,sem->value*-1);
+	printf("\n\033[41m sem_wait : \033[42m cpu%d for %s,value is%d\n\033[0m",_cpu(),sem->name,sem->value*-1);
 	while(sem->value<0) {
 		if(cur->park!=1)//no sleeped before or waken but no resourse
 			sem->sem_st[sem->top++]=cur;
@@ -123,7 +123,7 @@ void sem_wait(sem_t *sem) {
 void sem_signal(sem_t *sem) {
 	spin_lock(&sem->sem_lk);
 	sem->value++;
-	printf("\n\033[31m sem_signal: \033[32m cpu%d for %s\n\033[0m",_cpu(),sem->name);
+	printf("\n\033[41m sem_signal: \033[42m cpu%d for %s\n\033[0m",_cpu(),sem->name);
 	if(sem->value>=0)
 		sem->sem_st[--sem->top]->park=0;//runable
 	spin_unlock(&sem->sem_lk);
