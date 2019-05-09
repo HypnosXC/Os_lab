85869475773ce1bc675c36fa9f4d8ee36007f714
tracer-nju <tracer@nju.edu.cn>
2019-05-09 14:21:56 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  14:21:56 up  5:44,  4 users,  load average: 2.41, 1.78, 1.24

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -3,10 +3,12 @@
 
 int main() {
   _ioe_init();
+  printf("1\n");
   _cte_init(os->trap);
-
+  printf("2\n");
   // call sequential init code
   os->init();
+  printf("3\n");
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
