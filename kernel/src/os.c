02af54cc6b75b63f31a40002930236afacdbcdf6
tracer-nju <tracer@nju.edu.cn>
2019-05-09 23:45:40 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  23:45:40 up 15:07,  6 users,  load average: 0.33, 0.51, 0.45

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -40,16 +40,17 @@ static void os_run() {
     _yield();
   } 
 }
+
 void echo_task(void *name) {
 	device_t *tty= dev_lookup(name);
 	while(1) {
 		char line[128],text[128];
 		sprintf(text,"(%s) $ ", name);
-		tty_write(tty,text);
+		tty->ops->write(tty,0,text,sizeof(text));
 		int nread = tty->ops->read(tty,0,line,sizeof(line));
 		line[nread-1]='\0';
 		sprintf(text,"Echo: %s,\n",line);
-		tty_write(tty,text);
+		tty->ops->write(tty,0,text,strlen(text));
 	}
 }
 static _Context *os_trap(_Event ev, _Context *context) {
