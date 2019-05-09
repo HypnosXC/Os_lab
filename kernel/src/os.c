4c1c265affe2214fa7f23bbc6c751cd5cdee9ff2
tracer-nju <tracer@nju.edu.cn>
2019-05-09 20:46:28 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ea47e7827fc8496aba5809298f4beb03   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  20:46:28 up 12:08,  6 users,  load average: 0.02, 0.18, 0.37

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -42,7 +42,7 @@ static void os_run() {
 
 static _Context *os_trap(_Event ev, _Context *context) {
   kmt->spin_lock(&trap_lk);
-//  printf("\033[32mtrap in cpu=%d?\n\033[0m",_cpu());
+  printf("\033[32mtrap in cpu=%d?\n\033[0m",_cpu());
   task_t *cur=current_task();
  // printf("\n%s\n",cur->name);
   cur->context=context;
