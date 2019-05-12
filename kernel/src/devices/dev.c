4ee58d709e6cfc6eab371959cd801e059da264f6
tracer-nju <tracer@nju.edu.cn>
2019-05-12 23:33:39 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  23:33:39 up  1:35,  4 users,  load average: 0.55, 0.43, 0.38

--- kernel/src/devices/dev.c
+++ kernel/src/devices/dev.c
@@ -47,7 +47,7 @@ void input_task(void *arg);
 
 static void dev_init() {
   DEVICES(CREATE);
-//  DEVICES(INIT);
+  DEVICES(INIT);
 
   kmt->create(pmm->alloc(sizeof(task_t)), "input-task", input_task, NULL);
   kmt->create(pmm->alloc(sizeof(task_t)), "tty-task", tty_task, NULL);
