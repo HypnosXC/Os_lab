4a15987e23c4bfbab4212482b3275990e186350a
tracer-nju <tracer@nju.edu.cn>
2019-05-13 17:21:35 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  17:21:35 up 19:22,  5 users,  load average: 0.91, 1.48, 1.32

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -23,9 +23,9 @@ static void os_init() {
   _vme_init(pmm->alloc,pmm->free);
   dev->init();
   kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty1");
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty2");
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty3");
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty4");
+ // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty2");
+ // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty3");
+ // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty4");
   // vfs->init();
   //printf("\033[31m kmt finished!\n\033[0m");
 }
@@ -50,9 +50,9 @@ void echo_task(void *name) {
 	while(1) {
 		char line[128],text[128];
 		sprintf(text,"(%s) $ ", name);
-//		printf("ready write!\n");
+		printf("ready write!\n");
 		tty->ops->write(tty,0,text,strlen(text));
-//		printf("write over!\n");
+		printf("write over!\n");
 		int nread = tty->ops->read(tty,0,line,sizeof(line));
 		line[nread-1]='\0';
 		sprintf(text,"Echo: %s,\n",line);
@@ -60,7 +60,8 @@ void echo_task(void *name) {
 	}
 }
 static _Context *os_trap(_Event ev, _Context *context) {
-  kmt->spin_lock(&trap_lk);
+  //kmt->spin_lock(&trap_lk);
+  pushcli();
  // printf("\033[32mtrap in cpu=%d?\n\033[0m",_cpu());
   //printf("\ncpu#%d hold task %s\n",_cpu(),cur->name);
   _Context *ret=NULL;
@@ -79,12 +80,13 @@ static _Context *os_trap(_Event ev, _Context *context) {
 	assert(0);
   }
  // printf("\nIn trap now for cpu#%d, cnt is %d\n",_cpu(),cnt_cpu()); 
-  kmt->spin_unlock(&trap_lk);
+ // kmt->spin_unlock(&trap_lk);
   //printf("\033[32mtrap finished,with task=%s,eip=%d\n\033[0m",cur->name,ret->eip);
   if(ret==NULL) {
   	printf("\n\033[32m panic \003[33m trap return NULL to revise\033[0m\n");
 	assert(0);
   } 
+  popcli();
   return ret;
 }
 
