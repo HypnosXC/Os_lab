01b5ca46616a0d95ea0e621785d34922fda37115
tracer-nju <tracer@nju.edu.cn>
2019-05-13 16:46:57 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  16:46:57 up 18:48,  5 users,  load average: 1.00, 1.45, 1.46

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -23,7 +23,7 @@ static void os_init() {
   _vme_init(pmm->alloc,pmm->free);
   dev->init();
   kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty1");
- // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty2");
+  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty2");
  // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty3");
  // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty4");
   // vfs->init();
