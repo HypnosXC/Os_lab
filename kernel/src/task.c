fed92a5b1ee61accc6fd4da42aa4a0ff00324d48
tracer-nju <tracer@nju.edu.cn>
2019-05-13 15:02:52 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:02:52 up 17:04,  5 users,  load average: 0.62, 0.74, 0.88

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -244,10 +244,16 @@ _Context* context_switch(_Event e,_Context* c) {
 	}
 	if(ret==NULL) {
 		printf("\nno runable task! cpu=%d,total=%d\n",_cpu(),_ncpu());
+		for(int i=_cpu();i<TASK_SIZE;i+=_ncpu()) {
+			if(laoder[i]!=NULL)
+			printf("task=%s,park=%d,state=%d   ",loader[i]->name,loader[i]->park,loader[i]->state);
+		}
+		printf("\n");
 		int i=_cpu();
-		for(;i<TASK_SIZE;i+=_ncpu())
+		for(;i<TASK_SIZE;i+=_ncpu()) {
 			if(loader[i]!=NULL&&!strcmp(loader[i]->name,"null"))
 				break;
+		}
 //		printf("find task=%d,%s\n",i,current[i]->name);
 		if(i!=_cpu()) {
 			task_t* t=current[_cpu()];
