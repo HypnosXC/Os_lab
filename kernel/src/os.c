d9711d72756e28b8f020c848b121984beb9e0140
tracer-nju <tracer@nju.edu.cn>
2019-05-13 13:57:33 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  13:57:33 up 15:58,  4 users,  load average: 0.91, 0.75, 0.60

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -17,7 +17,9 @@ static void os_init() {
   pmm->init();
   kmt->spin_init(&trap_lk,"trap");
   kmt->spin_init(&irq_lk,"irq");
+  printf("kmt set\n");
   kmt->init();
+  printf("kmt seated!\n");
   _vme_init(pmm->alloc,pmm->free);
   dev->init();
 //  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty1");
