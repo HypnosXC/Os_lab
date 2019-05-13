ab6567ef63b00fc2618e35e322ec95e79c4b7cef
tracer-nju <tracer@nju.edu.cn>
2019-05-13 15:37:56 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  15:37:57 up 17:39,  5 users,  load average: 1.33, 1.17, 1.06

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -22,10 +22,10 @@ static void os_init() {
 //  printf("kmt seated!\n");
   _vme_init(pmm->alloc,pmm->free);
   dev->init();
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty1");
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty2");
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty3");
-  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty4");
+//  kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty1");
+  //kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty2");
+ // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty3");
+ // kmt->create(pmm->alloc(sizeof(task_t)),"print",echo_task,"tty4");
   // vfs->init();
   //printf("\033[31m kmt finished!\n\033[0m");
 }
