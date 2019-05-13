e57a7ac0951f7be5658eeb6f4f6d16ea15071973
tracer-nju <tracer@nju.edu.cn>
2019-05-13 18:18:31 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  18:18:31 up 20:19,  5 users,  load average: 0.78, 1.08, 1.08

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,4 +1,4 @@
-/*#include <kernel.h>
+#include <kernel.h>
 #include <klib.h>
 int main() {
   _ioe_init();
@@ -6,94 +6,6 @@ int main() {
   // call sequential init code
   os->init();
   printf("3\n");
-  _mpe_init(os->run); // all cores call os->run()
-  return 1;
-}*/
-#include <common.h>
-#include <kernel.h>
-#include <klib.h>
-
-typedef struct{
-  int n;
-  sem_t sem;
-}fib_t;
-
-sem_t sem_halt;
-
-const char f_name[][100] = {
-  "f0",
-  "f1",
-  "f2",
-  "f3",
-  "f4",
-  "f5",
-  "f6",
-  "f7",
-  "f8",
-  "f9",
-  "f10",
-  "f11",
-  "f12",
-  "f13",
-  "f14",
-  "f15",
-  "f16",
-  "f17",
-  "f18",
-  "f19",
-  "f20",
-  "f21",
-  "f22",
-  "f23",
-  "f24",
-};
-
-void fib(void *arg){
-  fib_t *f = arg;
-  if(f->n >= 2){
-    fib_t *f1 = pmm->alloc(sizeof(fib_t));
-    fib_t *f2 = pmm->alloc(sizeof(fib_t));
-
-    f1->n = f->n - 1;
-    f2->n = f->n - 2;
-    kmt->sem_init(&f1->sem, f_name[f->n - 1], 0);
-    kmt->sem_init(&f2->sem, f_name[f->n - 2], 0);
-    kmt->create(pmm->alloc(sizeof(task_t)), f_name[f->n - 1], fib, f1);
-    kmt->create(pmm->alloc(sizeof(task_t)), f_name[f->n - 2], fib, f2);
-	printf("f=%d\n",f->n);
-    kmt->sem_wait(&f1->sem);
-    kmt->sem_wait(&f2->sem);
-
-    f->n = f1->n + f2->n;
-
-    pmm->free(f1);
-    pmm->free(f2);
-  }
-  kmt->sem_signal(&f->sem);
-  kmt->sem_wait(&sem_halt);
-}
-
-void fib_c(void *arg){
-  long n = (long)arg;
-  fib_t *f = pmm->alloc(sizeof(fib_t));
-  f->n = n;
-  kmt->sem_init(&f->sem, "f", 0);
-  kmt->create(pmm->alloc(sizeof(task_t)), "", fib, f);
-  kmt->sem_wait(&f->sem);
-  printf("------------------%d--------------------\n", f->n);
-  pmm->free(f);
-  kmt->sem_wait(&sem_halt);
-}
-
-int main() {
-  _ioe_init();
-  _cte_init(os->trap);
-
-  // call sequential init code
-  os->init();
-  kmt->create(pmm->alloc(sizeof(task_t)), "ff", fib_c, (void *)3l);
-  printf("thead??");
-  kmt->sem_init(&sem_halt, "sem_halt", 0);
   _mpe_init(os->run); // all cores call os->run()
   return 1;
 }
