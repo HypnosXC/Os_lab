7ff2de6fcbd390a28813452999f7ccbbc3ccec93
tracer-nju <tracer@nju.edu.cn>
2019-05-13 16:57:53 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 8503488449f9447199fc5a2c6523903c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-47-generic       Architecture: x86-64  16:57:53 up 18:59,  5 users,  load average: 0.22, 0.51, 0.95

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -258,7 +258,7 @@ _Context* context_switch(_Event e,_Context* c) {
 			if(loader[i]!=NULL&&!strcmp(loader[i]->name,"null"))
 				break;
 		}
-		printf("cpu#%d find %d task=%s,eip=%d\n",_cpu(),i,current[i]->name,current[i]->context->eip);
+		printf("cpu#%d find %d task=%s,eip=%d\n",_cpu(),i,loader[i]->name,loader[i]->context->eip);
 		if(i!=_cpu()) {
 			task_t* t=current[_cpu()];
 			t->state=0;//runable now
