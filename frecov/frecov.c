61bf9b194e6e4e0a650b943ca9d824fbb39fe410
tracer-nju <tracer@nju.edu.cn>
2019-05-28 19:39:15 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 60ebc7d6b8964d7ba529a613b4c943e3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  19:39:16 up  1:07,  4 users,  load average: 0.16, 0.27, 0.42

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -10,7 +10,9 @@
 void *start;
 int BLO_NUM;
 int BLO_SZ;
+int BAS_GP;
 int FAT_NUM;
+int GP_BLO:
 int FAT_BLO;
 void* fat1;
 void *fat2;
@@ -20,9 +22,11 @@ void init(void *start) {
 	BLO_SZ=*((short*)(start+0x0b));
 	FAT_NUM=(int)(*((unsigned char *)(start+0x10)));
 	FAT_BLO=*((int *)(start+0x24));
+	BAS_GP=*((int *)(start+0x2c));
+	GP_BLO=(int)(*((unsigned char *)(start+0x0d)));
 	fat1=start+BLO_NUM*BLO_SZ;
 	fat2=fat1+FAT_BLO*BLO_SZ;
-	data=fat2+FAT_BLO*BLO_SZ;
+	data=fat2+FAT_BLO*BLO_SZ+(BAS_GP-2)*GP_BLO*BLO_SZ;
 	printf("f1=%x,f2=%x,data=%x\n",(int)(fat1-start),(int)(fat2-start),(int)(data-start));
 }
 int main(int argc, char *argv[]) {
