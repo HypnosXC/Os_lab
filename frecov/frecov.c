fd5e870c36b55956294d5cc7f1d0cc2bf6c88e12
tracer-nju <tracer@nju.edu.cn>
2019-05-28 19:02:05 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 60ebc7d6b8964d7ba529a613b4c943e3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  19:02:05 up 30 min,  4 users,  load average: 0.80, 1.77, 1.58

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -10,7 +10,17 @@
 char *start;
 int BLO_NUM;
 int BLO_SZ;
-
+int FAT_NUM;
+int FAT_BLO;
+void *fat1,fat2,data;
+void init(void *start) {
+	int jm=*((int *)(start+0x03));
+	BLO_NUM=*((short*)(start+0x0e));
+	BLO_SZ=*((short*)(start+0x0b));
+	FAT_NUM=(int)(*((unsigned char *)(start+0x10)));
+	FAT_BLO=*((int *)(start+0x24));
+	printf("jmp=%x\n",jm);
+}
 int main(int argc, char *argv[]) {
   int p=open(argv[1],O_RDONLY);
   if(p==-1) {
