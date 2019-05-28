8a354f6ebeb9fa92b95e78ff71a3bf29b12f9898
tracer-nju <tracer@nju.edu.cn>
2019-05-28 23:51:07 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 60ebc7d6b8964d7ba529a613b4c943e3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  23:51:07 up  5:19,  4 users,  load average: 0.34, 0.20, 0.31

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -68,7 +68,7 @@ void* file_read(void *head) {
 	//	for(int i=0;i<10;i++)
 	//		printf("%c",(char)fl_tab[num-1].name[i]);
 		wcstombs(pre,fl_tab[num-1].name,90);
-		printf("head=%x,%s,offset=%x\n",(int)(head-start)pre,(int)(fl_tab[num-1].start-start));
+		printf("head=%x,%s,offset=%x\n",(int)(head-start),pre,(int)(fl_tab[num-1].start-start));
 	}
 	return head+32;
 }
