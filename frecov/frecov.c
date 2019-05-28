e0d9ef316c3fd304fb509f99013145eb1015d53c
tracer-nju <tracer@nju.edu.cn>
2019-05-28 10:58:11 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: ef4888f702cd4e1588518b8835d8a415   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  10:58:11 up 48 min,  4 users,  load average: 1.18, 1.25, 1.27

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -7,7 +7,6 @@
 #include<stdlib.h>
 #include<string.h>
 int main(int argc, char *argv[]) {
-  return 0;
   int p=open(argv[0],O_RDONLY);
   if(p==-1) {
   	printf("\033[32m No such file to open! \033[0m \n");
