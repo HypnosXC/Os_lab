7d30a5fe5283dbd2f4c493d6b29037c47782b7b5
tracer-nju <tracer@nju.edu.cn>
2019-05-28 22:41:50 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 60ebc7d6b8964d7ba529a613b4c943e3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  22:41:50 up  4:10,  4 users,  load average: 0.85, 0.65, 0.47

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -45,7 +45,7 @@ void file_read(void *head) {
 	pos=(pos<<16)+*((short*)(head+0x1a));
 	fl_tab[num].start=fat2+(pos-2)*GP_BLO*BLO_SZ;
 	fl_tab[num].sz=*((int *)(head+0x1c));
-	memset(fl_tab[num].name,0,sizeof(char)*100);
+	memset(fl_tab[num].name,0,sizeof(char)*90);
 	memcpy(fl_tab[num].name,head,8);
 	num++;
 	printf("got file:%ls,offset=%x\n",fl_tab[num-1].name,(int)(fl_tab[num-1].start-start));
