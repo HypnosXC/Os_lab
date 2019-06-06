1081286986216a452a2fc80cf68a2f86be8e42a1
tracer-nju <tracer@nju.edu.cn>
2019-06-06 14:38:14 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 65fdd83245a143f3b6154eb776d84e4c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  14:38:14 up 45 min,  4 users,  load average: 0.82, 0.66, 0.65

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,2 +1,124 @@
 #include "kvdb.h"
+#include<sys/file.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<assert.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+void journaling(kvdb_t *db) {
+	int offset=8;
+	int max_off=lseek(db->fd,0,SEEK_END),doff=0;
+	while(offset<=max_off) {
+		lseek(db->fd,offset,SEEK_SET);
+		jmod* curs=alloc(sizeof(jmod)),cure=alloc(sizeof(jmod));
+		read(db->fd,cur,sizeof(jmod));
+		offset+=sizeof(jmod);
+		lseek(db->fd,offset,SEEK_SET);
+		offset+=sizeof(jmod);
+		read(db->fd,cure,sizeof(jmod));
+		if(curs->state==1&&cure->state==2) {//recovery able
+			char *buf=alloc(sizeof(char)*curs->size);
+			lseek(db->fd,offset,SEEK_SET);
+			offset+=size;
+			read(db->fd,buf,curs->size);
+			write(db->fd,buf,size);
+			offset+=size;
+			sync();
+			curs->state=3;
+			lseek(db->fd,offset-2*(size+sizeof(jmod)),SEEK_SET);
+			write(db->fd,curs,sizeof(jmod));
+			free(buf);
+			doff=offset;
+		} 
+		free(curs);
+		free(cure);
+	}
+	lseek(db->fd,0,SEEK_SET);
+	write(db->fd,&doff,sizeof(int));
+	sync();
+}
+int kvdb_open(kvdb_t *db,const char *filename) {
+	db->closed=0;
+	db->fd=open(filename,O_RDWR|O_CREAT,777);
+	flock(db->fd,LOCK_EX);
+	if(lseek(db->fd,0,SEEK_END)!=0) {
+		journaling(db);
+	}
+	else {
+		db->offset=8;
+		long long f=0;
+		write(db->fd,&f,sizeof(long long));
+	}
+	flock(db->fd,LOCK_UN);
+}
+int kvdb_close(kvdb_t *db) {
+	db->closed=1;
+}
+int kvdb_put(kvdb_t *db,const char * key,const char *value) {
+	if(db->closed==1) {
+		printf("error: dataset closed!\n");
+		return -1;
+	}
+	flock(db->fd,LOCK_EX);
+	off=lseek(db->fd,0,SEEK_END);
+	jmod s;
+	s->state=1;
+	s->size=strlen(value);
+	strcpy(s->name,key);
+	write(db->fd,&s,sizeof(jmod));
+	write(db->fd,&s,sizeof(jmod));
+	write(db->fd,value,strlen(value));
+	sync();
+	//create head jour and record data
+	s->state=2;
+	lseek(db->fd,off+sizeof(jmod),SEEK_SET);
+	write(db->fd,&s,sizeof(jmod));
+	sync();
+	// create end jour
+	lseek(db->fd,0,SEEK_END);
+	write(db->fd,value,sizeof(value));
+	// write data
+	s->state=3;
+	lseek(db->fd,off,SEEK_SET);
+	write(db->fd,&s,sizeof(jmod));
+	off=lseek(db->fd,0,SEEK_END);
+	lseek(db->fd,0,SEEK_SET);
+	write(db->fd,&off,sizeof(int));
+	// change  the maxoff
+	flock(db->fd,LOCK_UN);
+	return 0;
+}
+char* kvdb_get(kvdb_t *db,const char *key) {
+	offset=8;
+	int max_off=0,doff=0;
+	char* value=0;
+	read(db->fd,max_off,sizeof(int));
+	jmod *s=alloc(sizeof(jmod));
+	while(offset<=max_off) {
+		lseek(db->fd,offset,SEEK_SET);
+		read(db->fd,s,sizeof(jmod));
+		if(!strcmp(s->name,key))
+		   	doff=offset;
+		offset+=2*sizeof(jmod)+s->size*2;	
+	}
+	if(doff==0) {
+		free(s);
+		char pre[]="No such a key !\n";
+		return pre;
+	}
+	else {
+		lseek(db->fd,doff,SEEK_SET);
+		read(db->fd,s,sizeof(jmod));
+		doff+=s->size+sizeof(jmod)*2;
+		lseek(db->fd,doff,SEEK_SET);
+		value=alloc(s->size+1);
+		read(db->fd,value,s->size);
+		free(s);
+		return value;
+	}
+}
+
 
--- libkvdb/kvdb.h
+++ libkvdb/kvdb.h
@@ -1,13 +1,19 @@
 #ifndef __KVDB_H__
 #define __KVDB_H__
-
-
+typedef struct _jour_mod{
+	int state;//1 for head, 2 for end ,3 for used or not useful;
+	int size;//file size 
+	char name[130];
+}jmod;
 struct kvdb {
+	int fd;
+	int closed;
 };
 typedef struct kvdb kvdb_t;
-
-int kvdb_open(kvdb_t *db, const char *filename);
-int kvdb_close(kvdb_t *db);
+void journaling(kvdb *db);
+int kvdb_open(kvdb_t *db, const char *filename){
+	
+}int kvdb_close(kvdb_t *db);
 int kvdb_put(kvdb_t *db, const char *key, const char *value);
 char *kvdb_get(kvdb_t *db, const char *key);
 
