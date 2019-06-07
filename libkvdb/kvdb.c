b66973fada9879cc062f1633c370278753909396
tracer-nju <tracer@nju.edu.cn>
2019-06-08 01:48:11 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: c7848198a6a24b9fa6946a0fa8097ac7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  01:48:11 up  1:03,  4 users,  load average: 0.11, 0.16, 0.22

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -138,7 +138,7 @@ char* kvdb_get(kvdb_t *db,const char *key) {
 	while(offset<=max_off) {
 		lseek(db->fd,offset,SEEK_SET);
 		read(db->fd,s,sizeof(jmod));
-		printf("\033[32m offset=%d,name=%s,\033[0m\n",offset,s->name,strcmp(s->name,key));
+		printf("\033[32m offset=%d,name=%s,%d\033[0m\n",offset,s->name,strcmp(s->name,key));
 		if(!strcmp(s->name,key))
 		   	doff=offset;
 		offset+=2*sizeof(jmod)+s->size*2;	
