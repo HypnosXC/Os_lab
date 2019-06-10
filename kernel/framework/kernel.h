3166643479d22c293c0c01cb60dd99f96c75210e
tracer-nju <tracer@nju.edu.cn>
2019-06-10 14:39:22 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 376488de06d54aa1afdd971b4fd1f760   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  14:39:22 up 36 min,  4 users,  load average: 0.85, 0.91, 0.87

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -28,7 +28,9 @@ typedef struct {
 
 typedef struct task task_t;
 typedef struct spinlock spinlock_t;
+typedef struct inode inode_t;
 typedef struct semaphore sem_t;
+typedef struct filesystem filesystem_t;
 typedef struct {
   void (*init)();
   int (*create)(task_t *task, const char *name, void (*entry)(void *arg), void *arg);
@@ -40,7 +42,11 @@ typedef struct {
   void (*sem_wait)(sem_t *sem);
   void (*sem_signal)(sem_t *sem);
 } MODULE(kmt);
-
+typedef file {
+	int  refcnt;
+	inode_t *inode;
+	uint64_t offset;
+}file_t;
 typedef struct device device_t;
 typedef struct devops {
   int (*init)(device_t *dev);
@@ -50,5 +56,46 @@ typedef struct devops {
 typedef struct {
   void (*init)();
 } MODULE(dev);
-
+typedef struct fsops {
+	void (*init)(filesystem_t *fs,const char *name,device_t *dev);
+	inode_t *(*lookup)(filesystem_t *fs,const char *path,int flags);
+	int (*close)(inode_t *inode);
+}fsops_t;
+struct filesystem {
+	fsops_t *ops;
+	device_t *dev;
+};
+typedef struct inodeops{
+    int (*open)(file_t *file, int flags);
+  	int (*close)(file_t *file);
+ 	ssize_t (*read)(file_t *file, char *buf, size_t size);
+ 	ssize_t (*write)(file_t *file, const char *buf, size_t size);
+  	off_t (*lseek)(file_t *file, off_t offset, int whence);
+ 	int (*mkdir)(const char *name);
+ 	int (*rmdir)(const char *name);
+  	int (*link)(const char *name, inode_t *inode);
+  	int (*unlink)(const char *name);
+  	// 你可以自己设计readdir的功能
+}inodeops_t;
+typedef struct inode {
+  int refcnt;
+  void *ptr;
+  filesystem_t *fs;
+  inodeops_t *ops; 
+};
+typedef struct {
+	void (*init)();
+	int (*access)(const char * path,int mode);
+	int (*mount)(const char *path,filsystem_t *fs);
+	int (*unmont)(const char *path);
+	int (*mkdir)(const char *path);
+	int (*rmdir)(const char *path);
+	int (*link)(const char *oldpath,const char* newpath);
+	int (*unlink)(const char *path);
+	int (*open)(const char *path,int flags);
+	ssize_t (*read)(int fd,void *buf,size_t nbyte);
+	ssize_t (*write)(int fd,void *buf,size_t nbyte);
+	off_t (*lssek)(int fd,off_t offset,int whence);
+	int (*close)(int fd);
+} MODULE(vfs);
 #endif
