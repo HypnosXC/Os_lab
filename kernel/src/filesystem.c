3ad9d4a77b3f21efb1affe34df5cc0557fd8e955
tracer-nju <tracer@nju.edu.cn>
2019-06-11 20:10:58 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 9098dea13c9546b7ba0019b6a329d6ba   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  20:10:58 up  1:30,  4 users,  load average: 0.43, 0.51, 0.52

--- kernel/src/filesystem.c
+++ kernel/src/filesystem.c
@@ -175,7 +175,7 @@ inode_t * fs_lookup(filesystem_t *fs,const char *path,int flags) {
 			pre->size+=12;
 			int i=inode_create(fs,flags,(flags!=4),inode_op);
 			off_t addr=INODE_ENTRY+i*sizeof(inode);
-			basic_write(pre,pre->size,addr,sizeof(off_t));
+			basic_write(pre,pre->size,&addr,sizeof(off_t));
 			pre->size+=128-pre->size%128;
 		}
 
