dc0df9ee17c5cd6e60470eaf0810f3ea6c9c458c
tracer-nju <tracer@nju.edu.cn>
2019-06-14 14:11:34 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: c93c7de26a8c4ddba03a2ed4fb5f70f0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  14:11:34 up 8 min,  4 users,  load average: 0.51, 0.56, 0.31

--- kernel/src/filesystem.c
+++ kernel/src/filesystem.c
@@ -50,19 +50,15 @@ void add_inode(inode_t* dir,const char *name,inode_t *fl) {
 		printf("\033[42m Mkdir in a nondirectory!\033[0m\n");
 		assert(0);
 	}
-	char pname[110];
+	char pname[130];
 	memset(pname,0,sizeof(pname));
 	memcpy(pname,name,strlen(name));
-	basic_write(dir,dir->size,pname,100);
-	int f=0;
-	while(dir->size%128!=112)
-		basic_write(dir,dir->size,(char *)&f,sizeof(int));
+	basic_write(dir,dir->size,pname,112);
 	basic_write(dir,dir->size,(char *)&fl->pos,sizeof(off_t));
 	int ff=0;
 	basic_read(dir,dir->size-4,(char*)&ff,sizeof(off_t));
 	printf("\033[42m add_inode: originally size=%d,name=%s\033,off %d=%d[0m\n",dir->size,pname,ff,fl->pos);
-	while(dir->size%128!=0)
-		basic_write(dir,dir->size,(char *)&f,sizeof(int));
+	basic_write(dir,dir->size,pname+112,128-dir->size%128);
 	printf("\033[42m add_inode Now size is %d\033[0m\n",dir->size);
 }
 int inode_create(filesystem_t *fs,int prio,int type,inodeops_t *ops) {
