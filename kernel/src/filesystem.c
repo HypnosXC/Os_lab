ecdebc6790f6ff127cfd96fef89f3037d1209667
tracer-nju <tracer@nju.edu.cn>
2019-06-14 17:34:22 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: c93c7de26a8c4ddba03a2ed4fb5f70f0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  17:34:22 up  3:31,  4 users,  load average: 1.18, 1.14, 0.90

--- kernel/src/filesystem.c
+++ kernel/src/filesystem.c
@@ -183,7 +183,7 @@ int in_close(inode_t *pre) {
 	del_map(dev,DATA_MAP_ENTRY,i);
 	inode_t *mpre=pmm->alloc(sizeof(inode_t));
 	off_t poff=name_lookup(pre,"..");
-	dev->ops->read(dev,poff,&mpre,sizeof(inode_t));
+	dev->ops->read(dev,poff,mpre,sizeof(inode_t));
 	printf("\033[42m inode_close: pre inode is %p,size=%p\033[0m\n",poff,mpre->pos);
 	off_t doff=0,ioff=0;
 	while(doff<=mpre->size) {
