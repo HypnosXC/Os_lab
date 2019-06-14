e5b8dd4da33eb040b54f77be75854e036d85617f
tracer-nju <tracer@nju.edu.cn>
2019-06-14 10:17:21 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: fb718f761eb644898d3e2adcbec8cd98   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-48-generic       Architecture: x86-64  10:17:21 up 3 min,  4 users,  load average: 1.18, 0.70, 0.29

--- kernel/src/operation.c
+++ kernel/src/operation.c
@@ -20,7 +20,7 @@ char* ls_operation(const char *path) {
 	int end=vfs->lseek(fd,0,2);
 	printf("\033[42m end=%d\033[0m\n");
 	int doff=0,i=0;
-	while(doff!=end) {
+	while(doff<=end) {
 	    char *name=pmm->alloc(128);	
 		vfs->lseek(fd,doff,0);
 		vfs->read(fd,name,100);
