5458293bfc7e49b37d8ec6b6cd7c31fbe38735c2
tracer-nju <tracer@nju.edu.cn>
2019-06-16 21:21:44 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 0718398775ec4daea971dcd9b187dff8   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  21:21:44 up  1:25,  4 users,  load average: 0.80, 0.71, 0.61

--- kernel/src/filesystem.c
+++ kernel/src/filesystem.c
@@ -390,13 +390,13 @@ void vfs_init() {
 	fs_init(fs,"/",dev);
 	printf("init f\n");	
 	fs_lookup(fs,"/proc",4);
-//	fs_lookup(fs,"/dev",4);
-//	fs_lookup(fs,"/dev/ramdisk1",3);
-//	fs_lookup(fs,"/dev/null",2);
-//	fs_lookup(fs,"/dev/zero",1);
-//	fs_lookup(fs,"/dev/rand",0);
-//	fs_lookup(fs,"/proc/cpuinfo",5);
-//	fs_lookup(fs,"/proc/meminfo",5);
+	fs_lookup(fs,"/dev",4);
+	fs_lookup(fs,"/dev/ramdisk1",3);
+	fs_lookup(fs,"/dev/null",2);
+	fs_lookup(fs,"/dev/zero",1);
+	fs_lookup(fs,"/dev/rand",0);
+	fs_lookup(fs,"/proc/cpuinfo",5);
+	fs_lookup(fs,"/proc/meminfo",5);
 	fs_tab[0].ops=&fs_op;
 	printf("\033[42m where dead?\033[0m\n");
 }
