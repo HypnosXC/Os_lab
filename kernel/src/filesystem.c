b895604612a832fbeed08110e8123ee099029888
tracer-nju <tracer@nju.edu.cn>
2019-06-16 21:14:15 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 0718398775ec4daea971dcd9b187dff8   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  21:14:15 up  1:18,  4 users,  load average: 0.70, 0.75, 0.59

--- kernel/src/filesystem.c
+++ kernel/src/filesystem.c
@@ -387,7 +387,7 @@ void vfs_init() {
 	kmt->spin_init(fs_lk,"file system");
 	device_t *dev=dev_lookup("ramdisk0");
 	filesystem_t *fs=&fs_tab[0];
-	fs_init(&fs_tab[0],"/",dev);
+	fs_init(fs,"/",dev);
 	printf("init f\n");	
 //	fs_lookup(fs,"/proc",4);
 //	fs_lookup(fs,"/dev",4);
