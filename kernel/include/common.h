4c98b8c69c08e6716cf6d30ca273b3ea71dc4d03
tracer-nju <tracer@nju.edu.cn>
2019-07-01 00:09:16 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 1a1deb25b8d647dda5b776f0e1d689b7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  00:09:16 up 32 min,  5 users,  load average: 0.20, 0.62, 0.72

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -9,6 +9,12 @@
 //#define spin_lock(c) kmt->spin_lock(c)
 //#define spin_unlock(c) kmt->spin_unlock(c)
 //#define spin_init(c,name) kmt->spin_init(c,name)
+#define BLOCK_SIZE (off_t)2048
+#define INODE_MAP_ENTRY (off_t)0
+#define DATA_MAP_ENTRY (off_t)2048
+#define INODE_ENTRY (off_t)4096
+#define DATA_ENTRY (off_t)(4096+2048*sizeof(inode_t))
+#define FLSYS_NUM  10
 struct task {
 	char name[30];
 	file_t *flides[100];
