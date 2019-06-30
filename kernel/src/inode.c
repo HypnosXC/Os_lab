dcd68d599bc5ad58367b1abd8af1892137dbc19a
tracer-nju <tracer@nju.edu.cn>
2019-07-01 00:04:10 +0800

    Static hostname: sofsoul-RESCUER-R720-15IKBM          Icon name: computer-laptop            Chassis: laptop         Machine ID: f39bf23b9c414ede94f970cea853ee05            Boot ID: 1a1deb25b8d647dda5b776f0e1d689b7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-51-generic       Architecture: x86-64  00:04:10 up 27 min,  5 users,  load average: 0.77, 0.93, 0.81

--- kernel/src/inode.c
+++ kernel/src/inode.c
@@ -228,6 +228,7 @@ ssize_t devfs_write(file_t *file,const char *buf,size_t size) {
 char taskinfo[100];
 ssize_t proc_read(file_t *file,char *buf,size_t size) {
 	kmt->spin_lock(inode_lk);
+	return 0;
 	info_update();
 	ssize_t len=-1;
 	printf("\033[42mgot here proc_read!\n");
